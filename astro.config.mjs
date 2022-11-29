e75dcf795e72d12111ef2945ec8a395b7433f6cc
Amadeus <38767929+Mozart409@users.noreply.github.com>
2022-11-29 09:53:58 +0100

    feat: :sparkles: added more pages and deployment yaml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
-import sitemap from "@astrojs/sitemap";
-import prefetch from "@astrojs/prefetch";
+import tailwind from '@astrojs/tailwind'
+import image from '@astrojs/image'
+import sitemap from '@astrojs/sitemap'
+import prefetch from '@astrojs/prefetch'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mozart409.github.io',
+  base: '/astro-demo',
   integrations: [tailwind(), image(), sitemap(), prefetch()]
-});
+})
