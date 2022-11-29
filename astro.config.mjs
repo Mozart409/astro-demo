e22b695cd7b42a06e8fcda62dcf6bfa36563d3d1
Amadeus <38767929+Mozart409@users.noreply.github.com>
2022-11-29 10:04:05 +0100

    feat: :mag: added robots and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,11 @@ import tailwind from '@astrojs/tailwind'
 import image from '@astrojs/image'
 import sitemap from '@astrojs/sitemap'
 import prefetch from '@astrojs/prefetch'
+import robotsTxt from 'astro-robots-txt'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mozart409.github.io',
   base: '/astro-demo',
-  integrations: [tailwind(), image(), sitemap(), prefetch()]
+  integrations: [tailwind(), sitemap(), robotsTxt(), image(), prefetch()]
 })
